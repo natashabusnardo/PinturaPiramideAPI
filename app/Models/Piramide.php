a099ad3fd42e9dc5a743d2efdecec8a0771f8724
Natasha <natashabusnardo@hotmail.com>
2022-08-21 20:50:41 -0300

    first commit

---
+++ app/Models/Piramide.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Models;
+
+class Piramide {
+    double h;
+    double ab;
+}
---
+++ routes/web.php
@@ -0,0 +1,17 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/piramide/{ab}/{h}/{tt}', function ($ab, $h, $tt) {
+    return view('piramide', compact('h','ab','tt'));
+})->where(['ab' => '[0-9999.99]+', 'h' => '[0-9999.99]+', 'tt' => '[1-3]']);
